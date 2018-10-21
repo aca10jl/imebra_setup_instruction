b64a736065dafab570dce9f5ce2a16bf21fdde38
aca10jl <aca10jl@users.noreply.github.com>
2018-10-21 18:04:01 +0100

    Read ID from a dcm file

--- test.cpp
+++ test.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 int main() 
 {
-    cout << "Hello, World!";
-    unique_ptr<imebra::DataSet> loadedDataSet(imebra::CodecFactory::load("DicomFile.dcm"));
+    cout << "Hello, World!\n";
+    
+    unique_ptr<imebra::DataSet> loadedDataSet(imebra::CodecFactory::load("000001.dcm"));
+    wstring patientNameCharacter = loadedDataSet->getUnicodeString(imebra::TagId(imebra::tagId_t::PatientID_0010_0020), 0, L"");
+
+	wcout << "PatientID: " << patientNameCharacter << endl;
+
     return 0;
 }
